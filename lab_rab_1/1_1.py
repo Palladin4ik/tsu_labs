0b74bbdc5e29188b64cdd3c36c8414e8a00d5b17
Palladin4ik <64522831+Palladin4ik@users.noreply.github.com>
2024-12-18 23:43:29 +0300

    Update 1_1.py

--- lab_rab_1/1_1.py
+++ lab_rab_1/1_1.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
+import sys
 import random
 
 try:
-    A = random.randint(-10, 10)
-    print("Число A:", A)
-except Exception as e:
-    import sys
-    print(f"Ошибка: {e}", file=sys.stderr)
+    A = random.randint(-10, 10) #Генерация случайного числа
+    print("Число A:", A) #Вывод числа A
+except Exception as e: #Запись ошибок в файл error.txt
+    with open("error.txt", "a") as error_file:
+        error_file.write(f"Ошибка в 1_1.py: {e}\n")
