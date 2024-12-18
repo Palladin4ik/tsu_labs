00faddf0c390e89eb49a062ead17521136beb9ad
Palladin4ik <64522831+Palladin4ik@users.noreply.github.com>
2024-12-19 00:47:51 +0300

    Update 1_3.py

--- lab_rab_1/1_3.py
+++ lab_rab_1/1_3.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
+import sys
 import math
 
 try:
-    A = float(input("Введите число для вычисления квадратного корня: "))
-    
+    data = input()
+    A = float(data)
+
     if A < 0:
         raise ValueError("Нельзя вычислить квадратный корень из отрицательного числа!")
-    
+
     result = math.sqrt(A)
-    
+
     with open("output.txt", "w") as file:
         file.write(f"Квадратный корень числа {A}: {result}\n")
-    
+
     print("Результат записан в файл output.txt")
 except ValueError as ve:
+    with open("error.txt", "a") as error_file:
+        error_file.write(f"Ошибка преобразования данных в число: {ve}\n")
     print(f"Ошибка: {ve}", file=sys.stderr)
 except IOError as ioe:
+    with open("error.txt", "a") as error_file:
+        error_file.write(f"Ошибка записи в файл: {ioe}\n")
     print(f"Ошибка записи в файл: {ioe}", file=sys.stderr)
 except Exception as e:
+    with open("error.txt", "a") as error_file:
+        error_file.write(f"Ошибка: {e}\n")
     print(f"Ошибка: {e}", file=sys.stderr)
